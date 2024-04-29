1ca40df8c98eefb6f20271e33e2ecc85b31d8333
Bernardo Bruxel <144716083+bernardobru@users.noreply.github.com>
2024-04-29 11:11:41 -0300

    atividade01.js

--- atividade01.js
+++ atividade01.js
@@ -2,6 +2,8 @@ const prompt = require('prompt-sync')();//Declaração do prompt para o usuário
 
 let celsius;//Declaração da variável da temperatura em Graus Celsius
 
+console.log('CONVERSÃO DE TEMPERATURA: C° PARA F°');
+
 celsius = prompt("Digite a temperatura em C°: ");//Pergunta ao usuário qual o valor da temperatura que ele quer converter de Celsius para Fahrenheit 
 
 conversao = celsius * 1.8 + 32;//Declaração da variável que recebe a fórmula de conversão de Celsius para Fahrenheit
