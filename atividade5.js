15fee9af970f48205f3adf189cd7016cf8ef7265
Bernardo Bruxel <144716083+bernardobru@users.noreply.github.com>
2024-04-29 11:39:43 -0300

    atividade05.js

--- atividade5.js
+++ atividade5.js
@@ -1,11 +1,13 @@
 const prompt = require('prompt-sync')();
 
-let nota1 = parseFloat(prompt("Digite a nota do 1ª prova: "));
-let nota2 = parseFloat(prompt("Digite a nota da 2ª prova: "));
-let media = parseFloat((nota1 + nota2)/2);
+console.log("INSIRA O VALOR DE DUAS PROVAS PARA CALCULAR A MÉDIA SEMESTRAL");
 
-if (media > 6.0) {
-    console.log('PARABÉNS! Você foi aprovado');
-} else if (media < 6.0) {
+let nota1 = parseFloat(prompt("Digite a nota do 1ª prova: "));//Pede para que o usuário insira um valor para ser a 1° nota
+let nota2 = parseFloat(prompt("Digite a nota da 2ª prova: "));//Pede para que o usuário insira um valor para ser a 2° nota
+let media = parseFloat((nota1 + nota2)/2);//Faz o cálculo da média
+
+if (media > 6.0) {//Verifica se a média é maior que 6.0, caso for, imprime a seguinte mensagem:
+    console.log("Parabéns! Voce foi APROVADO!");
+} else if (media < 6.0) {//Caso a média seja menor que 6.0, imprime a seguinte mensagem:
     console.log('Você foi REPROVADO! Estude mais');    
 }
