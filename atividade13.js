f5ebfa93d993f723de71fcb77d093467f6c7e529
Bernardo Bruxel <144716083+bernardobru@users.noreply.github.com>
2024-04-29 16:45:33 -0300

    Update atividade13.js

--- atividade13.js
+++ atividade13.js
@@ -1,5 +1,7 @@
 const prompt = require("prompt-sync")();
 
+console.log("VEJA A TABUÁDA DE 5 NÚMEROS");
+
 for (let indice = 0; indice < 5; indice++) {
     let numero = parseInt(prompt("Digite um número: "));
     for (let multiplicador = 1; multiplicador <= numero; multiplicador++) {
