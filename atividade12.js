cbb3e0d7b5304786259f1421f6e466b155d555cb
Bernardo Bruxel <144716083+bernardobru@users.noreply.github.com>
2024-04-29 16:42:39 -0300

    Update atividade12.js

--- atividade12.js
+++ atividade12.js
@@ -1,4 +1,4 @@
-console.log("IMPRIMINDO NÚMEROS DIVIDIDOS POR 11 QUE DÃO RESTO 5")
+console.log("IMPRIMINDO NÚMEROS ENTRE 1000 E 1999 QUE DIVIDIDOS POR 11 DÃO RESTO 5")
 for (let num = 1000; num <= 1999; num++){
     if (num % 11 === 5) {
         console.log(num);
