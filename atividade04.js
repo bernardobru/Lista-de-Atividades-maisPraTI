5cae02a84c8ab45c88a70ad1e59574e22ccac321
Bernardo Bruxel <144716083+bernardobru@users.noreply.github.com>
2024-04-29 11:36:03 -0300

    atividade04.js

---
+++ atividade04.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+console.log("INSIRA O VALOR DE DUAS PROVAS PARA CALCULAR A MÉDIA SEMESTRAL");
+
+let nota1 = parseFloat(prompt("Digite a nota do 1ª prova: "));//Pede para que o usuário insira um valor para ser a 1° nota
+let nota2 = parseFloat(prompt("Digite a nota da 2ª prova: "));//Pede para que o usuário insira um valor para ser a 2° nota
+let media = parseFloat((nota1 + nota2)/2);//Faz o cálculo da média
+
+if (media > 6.0) {//Verifica se a média é maior que 6.0, caso for, imprime a seguinte mensagem:
+    console.log(`PARABÉNS! Você foi aprovado com média ${media}`);
+}
